6cc9e7fc42a63009fe6810edc590a04f1770aa66
evan-k-nygard <9326075+evan-k-nygard@users.noreply.github.com>
2023-12-11 12:07:49 -0500

    Add files via upload
    Minor annotations added to dbmanager.cpp

--- dbmanager.cpp
+++ dbmanager.cpp
@@ -8,36 +8,36 @@
 #include "dbmanager.h"
 
 DBManager::DBManager(const char* dbname, const char* k) {
+    // open up a new SQLite3 database by initiating sqlite3* db
+    // also initiate const char* key with k
     int r = sqlite3_open(dbname, &db);
-    if(r != 0) {
+    if(r != 0) { // couldn't open the database properly
         sqlite3_close(db);
     }
     key = k;
 }
 
 DBManager::~DBManager() {
+    // close the database
     sqlite3_close(db);
 }
 
-void DBManager::store(std::string tbl, std::vector<std::string> args) {
-    std::string query("INSERT INTO" );
-    query += tbl;
-    query += " VALUES (";
-    for(size_t i = 0; i < args.size(); i++) {
-        query += args[i]; // NEED TO ENCRYPT THIS
-        if(i < args.size() - 1) {
-            query += ", ";
-        }
-    }
-    query += ")";
-    sqlite3_exec(db, query.c_str(), NULL, NULL, NULL);
-}
-
 DBTable DBManager::prepared_query(std::string q, const ArgumentList& args) {
+    /*
+    * Execute a prepared query with respect to the currently active database.
+    * @arguments
+    * ~ q: contains a prepared query string
+    * ~ args: contains a list of arguments, which will be binded to the
+        prepared values in the query q
+    * @expects args.size() == number of '?'s in q
+    * @returns DBTable containing results of query on success, throws
+        std::runtime_error on failure
+    */
     sqlite3_stmt* pstmt;
     if(sqlite3_prepare_v2(db, q.c_str(), q.size(), &pstmt, NULL) != SQLITE_OK) {
         throw std::runtime_error("unable to prepare statement");
     }
+    // bind all arguments in the ArgumentList to the query
     for(size_t i = 0; i < args.size(); i++) {
         if(sqlite3_bind_text(pstmt, i+1, args[i].c_str(), -1, SQLITE_STATIC) != SQLITE_OK) {
             throw std::runtime_error("unable to bind argument");
@@ -71,7 +71,37 @@ DBTable DBManager::prepared_query(std::string q, const ArgumentList& args) {
         }
     }
 
+    // we've finished the query; clean up and return
     sqlite3_finalize(pstmt);
     return result;
 
+}
+
+void DBManager::store(std::string tbl, std::vector<std::string> args) {
+    /*
+    * Stores values contained in "args" within the table specified by "tbl"
+    * WARNING: this is an old function, vulnerable to SQL Injection attacks. 
+    * Will require major edits, or possibly future deletion. For now, do not
+    * use; replace with prepared_query instead
+    * 
+    * @arguments
+    * ~ tbl: a string containing the table name
+    * ~ args: a list of values to insert into the table
+    * @expects tbl and args contain sanitized input; tbl refers to an existing
+        table in the database; args.size() == number of columns in the table
+        that expect user input
+    * @returns void; the associated table now contains values specified in args
+    */
+    std::string query("INSERT INTO ");
+    query += tbl;
+    query += " VALUES (";
+    // add the values to the query
+    for(size_t i = 0; i < args.size(); i++) {
+        query += args[i]; // TODO: encrypt args[i]
+        if(i < args.size() - 1) {
+            query += ", ";
+        }
+    }
+    query += ")";
+    sqlite3_exec(db, query.c_str(), NULL, NULL, NULL);
 }
