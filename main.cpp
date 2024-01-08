c4e28dd2c0b85f99d5a3045ef421afcb65f8b96b
evan-k-nygard <9326075+evan-k-nygard@users.noreply.github.com>
2024-01-07 20:38:36 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,9 +6,15 @@
 
 int main(int argc, char** argv) {
 
-    std::string keystr("mypasswd");
-    CryptoPP::SecByteBlock key = crypto::keygen(keystr);
-    std::string ctext = crypto::encrypt("Hello, Raven! Tiger strength, speed, and power\n", key);
+    std::string uname;
+    std::string pwd;
+    std::cout << "Username: ";
+    std::cin >> uname;
+    std::cout << "Password: ";
+    std::cin >> pwd;
+
+    CryptoPP::SecByteBlock key = crypto::master_keygen(uname, crypto::hash(pwd));
+    std::string ctext = crypto::encrypt("Test encryption\n", key);
     std::cout << "encryption: " << ctext << '\n';
     std::cout << "decryption: " << crypto::decrypt(ctext, key) << '\n';
 }
